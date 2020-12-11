872f74c69c842033c96081303eb20a4720ab42a3
colethecoder <colethecoder@gmail.com>
2020-12-11 12:13:35 +0000

    Completed Day 7

--- src/Advent7.hs
+++ src/Advent7.hs
@@ -3,7 +3,7 @@ module Advent7
     ) where
 
 import System.IO ()
-import Data.List ( sort, nub, intersect )
+import Data.List ( sort, nub, intersect, isSubsequenceOf )
 import Text.ParserCombinators.Parsec
 import Paths_advent
 import Data.Map.Strict (Map)
@@ -15,10 +15,24 @@ advent7 = do
     contents <- readFile filepath   
     print $ case parseBags contents of
         Left err -> show err
-        Right r -> show $ h ["shiny gold"] r
+        Right r -> show $ length $ filter (\x -> x/="shiny gold") $ h ["shiny gold"] r
+    print $ case parseBags contents of
+        Left err -> show err
+        Right r -> show $ i "shiny gold" r
+
+i :: String -> [(String, [(Int, String)])] -> Int
+i col bags = 
+    let bag = head $ filter (\x -> col == fst x) bags in
+    sum $ map (\x -> (fst x) * (1 + (i (snd x) bags))) $ snd bag
+
 
 h :: [String] -> [(String, [(Int, String)])] -> [String]
-h inners bags = foldr (f inners) [] bags 
+h inners bags = 
+    let outers = sort $ foldr (f inners) [] bags in
+    if outers == [] || (isSubsequenceOf outers $ sort inners)
+    then inners
+    else h (sort $ nub $ inners ++ outers) bags
+
 
 f :: [String] -> (String, [(Int, String)]) -> [String] -> [String]
 f matches (key, val) acc = if g matches val then key : acc else acc
