16d5a45af67616790b9adacaee9e6ca84270d199
colethecoder@gmail.com <colethecoder@gmail.com>
2020-12-13 07:07:20 +0000

    Rough working Day 8

--- src/Advent8.hs
+++ src/Advent8.hs
@@ -13,7 +13,69 @@ advent8 :: IO ()
 advent8 = do  
     filepath <- getDataFileName "input8.txt"
     contents <- readFile filepath   
-    print $ parseInstructions contents
+    print $ case parseInstructions contents of
+        Left err -> show err
+        Right r  -> show $ f r (BootState 0 0 [])
+    print $ case parseInstructions contents of
+        Left err -> show err
+        Right r  -> show $ i (j 0 r) (BootState 0 0 [])
+
+j :: Int -> [Instruction] -> [[Instruction]]
+j pos ins =
+    if length ins == pos
+    then []
+    else 
+        if (isACC (ins !! pos))
+        then (j (pos +1) ins)
+        else (modifyNth pos switchNOPandJMP ins) : (j (pos +1) ins)
+
+isACC :: Instruction -> Bool
+isACC (ACC x) = True
+isACC _ = False
+
+switchNOPandJMP :: Instruction -> Instruction
+switchNOPandJMP (NOP x) = JMP x
+switchNOPandJMP (JMP x) = NOP x
+switchNOPandJMP (ACC x) = ACC x
+
+i :: [[Instruction]] -> BootState -> Maybe Int
+i [] st = Nothing
+i (x:xs) st =
+    case g x st of
+        Just j -> Just j
+        Nothing -> i xs st
+
+g :: [Instruction] -> BootState -> Maybe Int
+g ins st = 
+    if any (\x -> x == (position st)) $ visitedPositions st
+    then Nothing    
+    else 
+        if (length ins) == (position st)
+        then Just $ accumulator st
+        else g ins $ moveNext (ins !! (position st)) st
+
+f :: [Instruction] -> BootState -> Int
+f ins st = 
+    if any (\x -> x == (position st)) $ visitedPositions st
+    then accumulator st
+    else f ins $ moveNext (ins !! (position st)) st
+
+modifyNth :: Int -> (a -> a) -> [a] -> [a]
+modifyNth _ _ [] = []
+modifyNth n calc (x:xs)
+   | n == 0 = (calc x):xs
+   | otherwise = x : modifyNth (n-1) calc xs
+
+moveNext :: Instruction -> BootState -> BootState
+moveNext (NOP i) st = BootState (accumulator st) ((position st) + 1) ((position st) : (visitedPositions st))
+moveNext (JMP i) st = BootState (accumulator st) ((position st) + i) ((position st) : (visitedPositions st))
+moveNext (ACC i) st = BootState ((accumulator st) + i) ((position st) + 1) ((position st) : (visitedPositions st))
+
+data BootState = BootState {
+    accumulator      :: Int
+  , position         :: Int
+  , visitedPositions :: [Int] 
+} deriving Show
 
 eol :: GenParser Char st Char
 eol = char '\n'
